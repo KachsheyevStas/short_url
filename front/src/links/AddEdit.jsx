2bfd416fe9265800cab33a9c85b4ec9dde973a8d
KachsheyevStas <kachsheyev.stas@gmail.com>
2024-05-19 12:01:42 +0500

    Изменено регулярное выражение для проверки адреса

--- front/src/links/AddEdit.jsx
+++ front/src/links/AddEdit.jsx
@@ -16,14 +16,14 @@ function AddEdit() {
     const dispatch = useDispatch();
     const link = useSelector(x => x.links?.item);
 
-    // form validation rules 
     const validationSchema = Yup.object().shape({
         originalUrl: Yup.string().required('Original Url is required')
         .matches(
-            /((http|https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/,
+            /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/,
             'Enter correct url!'
         )
         .required('Please enter website'),
+
     });
     const formOptions = { resolver: yupResolver(validationSchema) };
 
@@ -71,7 +71,7 @@ function AddEdit() {
                             Save
                         </button>
                         <button onClick={() => reset()} type="button" disabled={isSubmitting} className="btn btn-secondary">Reset</button>
-                        <Link to="/links" className="btn btn-link">Cancel</Link>
+                        {title == 'Edit Link' && <Link to="/links" className="btn btn-link">Cancel</Link>}
                     </div>
                 </form>
             }
